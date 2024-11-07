7a09c06a4eff415c8e7df2096069d5148ecf2f74
juliancape <juliancape360@gmail.com>
2024-11-06 19:13:51 -0500

    Estructura inicial con mas homescreen

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,57 +1,96 @@
-// HomeScreen.js
-import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet, Alert, TouchableOpacity, TextInput } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, FlatList, Keyboard } from 'react-native';
 import { auth } from './firebase/firebaseConfig';
 import { signOut } from "firebase/auth";
 import Icon from 'react-native-vector-icons/Ionicons';
+import MapView from 'react-native-maps';
 
 const HomeScreen = ({ navigation }) => {
   const [inputText, setInputText] = useState('');
+  const [alerts, setAlerts] = useState([
+    { id: '1', title: 'Título 1', subtitle: 'Subtítulo', description: 'Descripción' },
+    { id: '2', title: 'Título 2', subtitle: 'Subtítulo', description: 'Descripción' },
+    // Agrega más alertas aquí según sea necesario
+  ]);
+  const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
+
+  useEffect(() => {
+    // Detecta cuando el teclado se muestra o se oculta
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => setIsKeyboardVisible(true)
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => setIsKeyboardVisible(false)
+    );
+
+    // Limpia los listeners cuando el componente se desmonta
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
+    };
+  }, []);
 
   const handleSignOut = () => {
     signOut(auth)
-      .then(() => {
-        navigation.replace('Login');
-      })
-      .catch(error => {
-        console.error("Error al cerrar sesión:", error);
-        Alert.alert("Error", "No se pudo cerrar sesión. Inténtalo de nuevo más tarde.");
-      });
-  };
-
-  const handleNavigateTo = (screen) => {
-    navigation.navigate(screen);
+      .then(() => navigation.replace('Login'))
+      .catch(error => Alert.alert("Error", "No se pudo cerrar sesión. Inténtalo de nuevo más tarde."));
   };
 
   const handleAdd = () => {
     navigation.navigate('AlertScreen');
   };
 
+  const handleNavigateTo = (screen) => {
+    navigation.navigate(screen);
+  };
+
+  const renderAlertItem = ({ item }) => (
+    <View style={styles.alertCard}>
+      <Text style={styles.alertTitle}>{item.title}</Text>
+      <Text style={styles.alertSubtitle}>{item.subtitle}</Text>
+      <Text style={styles.alertDescription}>{item.description}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
+      <MapView style={styles.map} initialRegion={{ latitude: 4.6097, longitude: -74.0817, latitudeDelta: 0.1, longitudeDelta: 0.1 }} />
+      
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
-          placeholder="Ingresa tu texto aquí"
+          placeholder="Bogotá, Colombia"
           value={inputText}
           onChangeText={setInputText}
         />
-        <TouchableOpacity style={styles.continueButton} onPress={handleAdd}>
-          <Text style={styles.buttonText}>+</Text>
+        <TouchableOpacity style={styles.addButton} onPress={handleAdd}>
+          <Icon name="add" size={24} color="#fff" />
         </TouchableOpacity>
       </View>
-      <Text style={styles.welcomeText}>¡Bienvenido a AlertCity!</Text>
-      <Button title="Cerrar sesión" onPress={handleSignOut} />
-      <View style={styles.footer}>
-        <TouchableOpacity style={styles.button} onPress={() => handleNavigateTo('Explora')}>
-          <Icon name="search" size={30} />
-          <Text style={styles.buttonText}>Explora</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleNavigateTo('ProfileScreen')}>
-          <Icon name="person" size={30} />
-          <Text style={styles.buttonText}>Perfil</Text>
-        </TouchableOpacity>
-      </View>
+      
+      <Text style={styles.alertCount}>Se encontraron {alerts.length} alertas</Text>
+      <FlatList
+        data={alerts}
+        renderItem={renderAlertItem}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.alertList}
+      />
+
+      {/* Solo muestra el footer si el teclado no está visible */}
+      {!isKeyboardVisible && (
+        <View style={styles.footer}>
+          <TouchableOpacity style={styles.button} onPress={() => handleNavigateTo('Explora')}>
+            <Icon name="search" size={30} />
+            <Text style={styles.buttonText}>Explora</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => handleNavigateTo('ProfileScreen')}>
+            <Icon name="person" size={30} />
+            <Text style={styles.buttonText}>Perfil</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 };
@@ -59,56 +98,79 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
     backgroundColor: '#f8f8f8',
-    padding: 10,
+  },
+  map: {
+    height: '40%', // Ajusta la altura del mapa
   },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    width: '100%',
-    marginBottom: 20,
+    padding: 10,
+    marginHorizontal: 10,
+    marginTop: -20,
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 3,
   },
   input: {
     flex: 1,
-    borderWidth: 1,
-    borderColor: '#E4D1F9', // Color de borde morado
-    borderRadius: 20, // Bordes más circulares
     paddingHorizontal: 10,
-    height: 40,
-    marginRight: 10,
   },
-  continueButton: {
+  addButton: {
     backgroundColor: '#E4D1F9',
-    paddingVertical: 6,
-    borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 40,
-    height: 40,
-    alignSelf: 'center',
+    borderRadius: 20,
+    padding: 10,
   },
-  welcomeText: {
-    fontSize: 24,
-    marginBottom: 20,
+  alertCount: {
+    fontSize: 16,
+    marginVertical: 10,
+    marginLeft: 20,
+  },
+  alertList: {
+    paddingHorizontal: 10,
+  },
+  alertCard: {
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 10,
+    marginVertical: 5,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 3,
+  },
+  alertTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  alertSubtitle: {
+    fontSize: 14,
+    color: '#666',
+  },
+  alertDescription: {
+    fontSize: 12,
+    color: '#999',
   },
   footer: {
     position: 'absolute',
-    bottom: 20,
+    bottom: 0,
     flexDirection: 'row',
     justifyContent: 'space-around',
     width: '100%',
-    padding: 10,
+    paddingVertical: 10,
     backgroundColor: '#ffffff',
-    elevation: 2,
+    borderTopWidth: 1,
+    borderColor: '#e0e0e0',
   },
   button: {
     alignItems: 'center',
   },
   buttonText: {
-    marginTop: 5,
-    fontSize: 16,
+    fontSize: 14,
   },
 });
 
